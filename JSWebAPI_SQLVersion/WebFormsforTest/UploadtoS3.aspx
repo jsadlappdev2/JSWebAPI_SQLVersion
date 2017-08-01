2cd05f2af9c9e482f8d4cfee87cb64001e97bb7d
jsadlappdev@gmail.com <jsadlappdev@gmail.com>
2017-08-01 16:01:28 +0930

    added upload to amazon s3 form.

---
+++ JSWebAPI_SQLVersion/WebFormsforTest/UploadtoS3.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UploadtoS3.aspx.cs" Inherits="JSWebAPI_SQLVersion.WebFormsforTest.UploadtoS3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="Button1" runat="server" Height="36px" OnClick="Button1_Click" Text="Upload" Width="309px" />
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ JSWebAPI_SQLVersion/WebFormsforTest/UploadtoS3.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+
+
+namespace JSWebAPI_SQLVersion.WebFormsforTest
+{
+    public partial class UploadtoS3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            string localFilePath = "C:\\Jerry Shen\\testfolder\\sea.JPG";
+            FileStream fs = File.OpenRead(localFilePath);
+
+            bool uploadstatus =sendMyFileToS3(fs, "awss3dailylifehelperapp", "images", "sea.JPG");
+            if (uploadstatus)
+            {
+                Label1.Text = "upload successfully!";
+            }
+            else
+            {
+
+                Label1.Text = "upload failed!";
+            }
+        }
+
+      
+
+        public bool sendMyFileToS3(System.IO.Stream localFilePath, string bucketName, string subDirectoryInBucket, string fileNameInS3)
+        {
+            // input explained :
+            // localFilePath = we will use a file stream , instead of path
+            // bucketName : the name of the bucket in S3 ,the bucket should be already created
+            // subDirectoryInBucket : if this string is not empty the file will be uploaded to
+            // a subdirectory with this name
+            // fileNameInS3 = the file name in the S3
+            // create an instance of IAmazonS3 class ,in my case i choose RegionEndpoint.EUWest1
+            // you can change that to APNortheast1 , APSoutheast1 , APSoutheast2 , CNNorth1
+            // SAEast1 , USEast1 , USGovCloudWest1 , USWest1 , USWest2 . this choice will not
+            // store your file in a different cloud storage but (i think) it differ in performance
+            // depending on your location
+
+
+            // IAmazonS3 client = new AmazonS3Client("Your Access Key", "Your Secrete Key", Amazon.RegionEndpoint.USWest2);
+            IAmazonS3 client = new AmazonS3Client("AKIAJSJNUQCB7UEPLAFQ", "qWWpCUBNLo1ujl8We9Iz8gETklZxY2YmV5ImCDtT", Amazon.RegionEndpoint.APSoutheast2);
+
+            // create a TransferUtility instance passing it the IAmazonS3 created in the first step
+            TransferUtility utility = new TransferUtility(client);
+            // making a TransferUtilityUploadRequest instance
+            TransferUtilityUploadRequest request = new TransferUtilityUploadRequest();
+
+            if (subDirectoryInBucket == "" || subDirectoryInBucket == null)
+            {
+                request.BucketName = bucketName; //no subdirectory just bucket name
+            }
+            else
+            {   // subdirectory and bucket name
+                request.BucketName = bucketName + @"/" + subDirectoryInBucket;
+            }
+            request.Key = fileNameInS3; //file name up in S3
+                                        //request.FilePath = localFilePath; //local file name
+            request.InputStream = localFilePath;
+            request.CannedACL = S3CannedACL.PublicReadWrite;
+            try
+            {
+                utility.Upload(request); //commensing the transfer
+                return true; //indicate that the file was sent
+            }
+            catch(Exception err)
+            {
+                Label2.Text = "Error: " + err.Message.ToString();
+                return false; //indicate that the file was sent
+
+            }
+
+        }
+    }
+}
